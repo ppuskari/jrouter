ppuskari/jrouter

commit e88d7541ee4cb847c3c7e1f343c4e26f8a9f6fef

cleanup: use WaitGroup.Go

main.go
@@ -242,8 +242,7 @@ func main() {
 	var resolverWG sync.WaitGroup
 	peerCh := make(chan string)
 	for range runtime.GOMAXPROCS(0) {
-		resolverWG.Add(1)
-		go func() {
+		resolverWG.Go(func() {
 			defer resolverWG.Done()
 
 			for {
@@ -285,7 +284,7 @@ func main() {
 					continue
 				}
 			}
-		}()
+		})
 	}
 
 	for _, peerStr := range cfg.Peers {

commit b93d857e450923ad6e0318d8ed85a5ef06975531

fix: panic in resolverWG goroutine introduced in e88d7541

main.go
@@ -243,8 +243,6 @@ func main() {
 	peerCh := make(chan string)
 	for range runtime.GOMAXPROCS(0) {
 		resolverWG.Go(func() {
-			defer resolverWG.Done()
-
 			for {
 				var peerStr string
 				select {